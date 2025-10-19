ad52c9ef205f336cc59701c855620eda0392d9be
isaac.liu <tearsofphoenix@icloud.com>
2025-10-19 15:57:39 +0800

    feat: sentry client

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import "@/instrumentation-client"
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { SessionValidationResult } from "@/types"
